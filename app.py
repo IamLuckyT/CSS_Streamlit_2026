a6381adfaee06d79b01216f08bfea8af841d8174
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2026-02-03 11:15:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ Created on Tue Jan 27 17:32:56 2026
 
 import streamlit as st
 
+st.title("Profile Page with Projects made in Github")
+
+# Collect basic information
+name = "Letlhogonolo Mothoagae"
+field = "Full Stack Developer"
+institution = "North West University"
+
 st.write("Hello2")
 
 st.title("Title heading")
@@ -17,5 +24,6 @@ st.header("Number selection")
 
 number = st.slider("Pick a number", 1, 100)
 st.write(f"You picked: {number}")
+
 
 st.write("visit us @Plug Connect Solutions")
